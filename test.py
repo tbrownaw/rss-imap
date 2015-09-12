73866a439888e27e134ed11f3048bde340bea968
Tim Brownawell <tbrownaw@prjek.net>
2015-09-12 13:35:52 -0500

    It more-or-less works now. Need error handling, prettification, code cleanup, etc.

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import config
 
 import email
+from email.mime.text import MIMEText
 import imaplib
 import re
 
@@ -52,9 +53,15 @@ class FeedConfig:
     def __init__(self, dat):
         self.Folder = dat['Folder Name']
         self.URL = dat['Feed URL']
-
     def __repr__(self):
         return ("{ Folder: %s; URL: %s }" % (self.Folder, self.URL))
+    def quoted_folder(self):
+        return '"RSS/%s"' % self.Folder
+    def create_folder(self):
+        print('Creating folder for %s' % Folder)
+        M.create(self.quoted_folder())
+    def select_folder(self):
+        M.select(self.quoted_folder())
 
 def feed_configs_from_string(dat):
     for item in yaml.load_all(dat):
@@ -81,16 +88,35 @@ print(feeds)
 # Ensure folders exist
 for feed in feeds:
     if not any(f == 'RSS/' + feed.Folder for f in names):
-        print('Creating folder for %s' % feed.Folder)
-        M.create('"RSS/%s"' % feed.Folder)
+        feed.create_folder()
 
+def rss_item_to_email(item):
+    try:
+        text = item.link + "\r\n\r\n" + item.summary
+        email = MIMEText(text, "html")
+        email['Subject'] = item.title
+        email['From'] = item.author
+        email['Message-Id'] = item.get('id', item.link)
+        email['Date'] = item.published if 'published' in item else item.updated if 'updated' in item else item.created
+        return email
+    except:
+        print("***ERROR while processing this item:\n", item)
+        raise
 
 for feed in feeds:
     content = feedparser.parse(feed.URL)
     print(content.channel)
     print(len(content.entries))
-    for item in content.entries:
-        print(item.id, item.link)
-
+    feed.select_folder()
+    def emails_to_add():
+        for item in content.entries:
+            email = rss_item_to_email(item)
+            existing = search('HEADER', 'Message-Id', email['Message-Id'])
+            if not any(existing):
+                yield email
+    for email in emails_to_add():
+        print("%s\n" % email)
+        typ, detail = M.append(feed.quoted_folder(), '', '', str(email).encode('utf-8'))
+        print("Append result: %s %s\n\n\n" % (typ, detail))
 
 M.logout()
