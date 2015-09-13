c4d7b1c498c2ea8d5a24e7a6a874bd426334f48b
Tim Brownawell <tbrownaw@prjek.net>
2015-09-13 17:27:00 -0500

    More refactoring of things.

--- test.py
+++ test.py
@@ -18,6 +18,7 @@ class ImapWrapper:
     def __init__(self, host, user, pw):
         self.M = imaplib.IMAP4_SSL(host)
         self.M.login(user, pw)
+        self._selected_folder = None
         self._update_folders()
     def logout(self):
         self.M.logout()
@@ -50,28 +51,43 @@ class ImapWrapper:
             typ, listing = self.M.search(None, *args)
         except:
             raise IMAPError('Search failed with args "%s"' % str(args))
+        if typ != "OK":
+            raise IMAPError('search() failed: %s' % listing)
         for lst in listing:
             dd = lst.decode('US-ASCII')
             for m in dd.split(' '):
                 if m:
                     yield m
-    # FIXME uses the context folder
-    def fetch(self, *args):
-        return self.M.fetch(*args)
+    def fetch_messages(self, folder, *search_args):
+        self.select_folder(folder)
+        for num in self.search(*search_args):
+            typ, dat = self.M.fetch(num, '(RFC822)')
+            if typ != "OK":
+                raise IMAPError('Could not fetch searched message: %s' % dat)
+            msg = email.message_from_string(dat[0][1].decode('UTF-8'))
+            yield msg
+    def have_message_with_id(self, folder, msgid):
+        self.select_folder(folder)
+        res = list(self.search('HEADER', 'Message-Id', msgid, 'NOT', 'DELETED'))
+        return any(res)
     def append(self, *args):
         return self.M.append(*args)
-    def try_select_folder(self, name):
+    # FIXME sets the context folder
+    def select_folder(self, name):
+        if self._selected_folder == name:
+            return
         typ, dtl = self.M.select('"' + name + '"')
-        return typ == "OK"
-    def select_create_folder(self, name):
-        """Select the named folder, creating it if needed (and subscribing to it)."""
+        if typ != "OK":
+            raise IMAPError('Could not select folder "%s": %s' % (name, dtl))
+        self._selected_folder = name
+    def create_subscribe_folder(self, name):
         created = self.ensure_folder(name)
         if created:
             typ, dtl = self.M.subscribe('"' + name + '"')
             if typ != "OK":
                 raise IMAPError("Could not subscribe to folder %s" % name)
-        if not self.try_select_folder(name):
-            raise IMAPError("Could not select folder %s" % name)
+
+
 
 W = ImapWrapper(config.hostname, config.username, config.password)
 
@@ -94,12 +110,7 @@ def feed_configs_from_string(dat):
         yield FeedConfig(item)
 
 def read_email_config():
-    if not W.try_select_folder('.config'):
-        raise Exception("No .config folder available on IMAP server.")
-    for num in W.search('SUBJECT', 'rss-imap', 'NOT', 'DELETED'):
-        typ, dat = W.fetch(num, '(RFC822)')
-        msg_str = dat[0][1].decode('UTF-8')
-        msg = email.message_from_string(msg_str)
+    for msg in W.fetch_messages('.config', 'SUBJECT', 'rss-imap', 'NOT', 'DELETED'):
         if msg.is_multipart:
             for part in filter(lambda p: 'Folders' in p.get_param('Name', '(none)'), msg.get_payload()):
                 ss = part.get_payload(None, True).decode('UTF-8')
@@ -156,13 +167,11 @@ def rss_item_to_email(item):
 
 for feed in feeds:
     content = feedparser.parse(feed.URL)
-    #print(content.channel)
-    W.select_create_folder('RSS/' + feed.Folder)
+    W.create_subscribe_folder('RSS/' + feed.Folder)
     def emails_to_add():
         for item in content.entries:
             email = rss_item_to_email(item)
-            existing = W.search('HEADER', 'Message-Id', email['Message-Id'])
-            if not any(existing):
+            if not W.have_message_with_id('RSS/' + feed.Folder, email['Message-Id']):
                 yield email
     for email in emails_to_add():
         if email['Subject'] == '':
@@ -170,6 +179,9 @@ for feed in feeds:
         if email['Date'] == '':
             raise Error('Blank Date')
         typ, detail = W.append(feed.quoted_folder(), '', '', str(email).encode('utf-8'))
-        print('Append result for "%s": %s %s' % (email['Subject'], typ, detail))
+        if typ != "OK":
+            raise IMAPError('Could not add item: %s' % detail)
+        print('Feed "%s", new item "%s"' % (feed.Folder, email['Subject']))
+
 
 W.logout()
