ac686122bc3b6e19142f8834f56a542d2945ca58
Timothy Brownawell <tbrownaw@prjek.net>
2015-09-21 06:44:17 -0500

    Move top-level code into a function.

--- test.py
+++ test.py
@@ -89,11 +89,6 @@ class ImapWrapper:
 
 
 
-W = ImapWrapper(config.hostname, config.username, config.password)
-
-W.ensure_folder('.config')
-W.ensure_folder('RSS/')
-
 
 
 class FeedConfig:
@@ -109,8 +104,8 @@ def feed_configs_from_string(dat):
     for item in yaml.load_all(dat):
         yield FeedConfig(item)
 
-def read_email_config():
-    for msg in W.fetch_messages('.config', 'SUBJECT', 'rss-imap', 'NOT', 'DELETED'):
+def read_email_config(wrap):
+    for msg in wrap.fetch_messages('.config', 'SUBJECT', 'rss-imap', 'NOT', 'DELETED'):
         if msg.is_multipart:
             for part in filter(lambda p: 'Folders' in p.get_param('Name', '(none)'), msg.get_payload()):
                 ss = part.get_payload(None, True).decode('UTF-8')
@@ -120,7 +115,6 @@ def read_email_config():
             for fc in feed_configs_from_string(msg.get_payload()):
                 yield fc
 
-feeds = list(read_email_config())
 
 # Cribbing things from StackOverflow is fun. :)
 def strip_html(dat):
@@ -138,13 +132,16 @@ def strip_html(dat):
     ts.close()
     return ts.result()
 
+def item_message_id(item):
+    return item.get('id', item.link).replace(' ', '_')
+
 def rss_item_to_email(item):
     try:
         text = '<p>Item Link: <a href="%s">%s</a></p><br>%s' % (item.link, item.link, item.summary)
         email = MIMEText(text, "html")
         email['Subject'] = strip_html(item.title)
         email['From'] = item.get('author', '(Author Not Provided)')
-        email['Message-Id'] = item.get('id', item.link).replace(' ', '_')
+        email['Message-Id'] = item_message_id(item)
         if 'published' in item:
             date = item.published
             date_parts = item.published_parsed
@@ -165,23 +162,34 @@ def rss_item_to_email(item):
         print("***ERROR while processing this item:\n", item)
         raise
 
-for feed in feeds:
-    content = feedparser.parse(feed.URL)
-    W.create_subscribe_folder('RSS/' + feed.Folder)
-    def emails_to_add():
-        for item in content.entries:
-            email = rss_item_to_email(item)
-            if not W.have_message_with_id('RSS/' + feed.Folder, email['Message-Id']):
-                yield email
-    for email in emails_to_add():
-        if email['Subject'] == '':
-            raise Error('Blank Subject')
-        if email['Date'] == '':
-            raise Error('Blank Date')
-        typ, detail = W.append(feed.quoted_folder(), '', '', str(email).encode('utf-8'))
-        if typ != "OK":
-            raise IMAPError('Could not add item: %s' % detail)
-        print('Feed "%s", new item "%s"' % (feed.Folder, email['Subject']))
 
+def run_once():
+    W = ImapWrapper(config.hostname, config.username, config.password)
 
-W.logout()
+    W.ensure_folder('.config')
+    W.ensure_folder('RSS/')
+
+    feeds = list(read_email_config(W))
+
+    for feed in feeds:
+        content = feedparser.parse(feed.URL)
+        if content.bozo:
+            print("Feed %s had bozo set for '%s'" % (feed.Folder, content.bozo_exception))
+        W.create_subscribe_folder('RSS/' + feed.Folder)
+        def emails_to_add():
+            for item in content.entries:
+                if not W.have_message_with_id('RSS/' + feed.Folder, item_message_id(item)):
+                    yield rss_item_to_email(item)
+        for email in emails_to_add():
+            if email['Subject'] == '':
+                raise Error('Blank Subject')
+            if email['Date'] == '':
+                raise Error('Blank Date')
+            typ, detail = W.append(feed.quoted_folder(), '', '', str(email).encode('utf-8'))
+            if typ != "OK":
+                raise IMAPError('Could not add item: %s' % detail)
+            print('Feed "%s", new item "%s"' % (feed.Folder, email['Subject']))
+
+    W.logout()
+
+run_once()
